a7504c1d01f94b6eee8782c5e0fb5193475b7941
Gustavo <gustavopregigueiro6@gmail.com>
2023-03-03 21:45:06 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Cananéia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe,");
